4f4d870e5bbc455208010828db88192bee729d76
shahzaibansari123 <ansarishahzaib567@gmail.com>
2022-03-20 22:34:32 +0500

    working on it

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
-import React from 'react';
+import React from "react";
+import Header from "./components/Header/Header";
+import List from "./components/List/List";
+import Map from "./components/Map/Map";
+
+import { CssBaseline, Grid } from "@material-ui/core";
 
 export default function App() {
-  return <div></div>;
+  return (
+    <>
+      <CssBaseline />
+      <Header />
+      <Grid container spacing={3} style={{ width: "100%" }}>
+        <Grid item xs={12} md={4}>
+          <List />
+        </Grid>
+        <Grid item xs={12} md={8}>
+          <Map />
+        </Grid>
+      </Grid>
+
+      {/* <h1>helloworld</h1> */}
+    </>
+  );
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import react from "react"
-import reactDOM from "react-dom"
+import ReactDOM from "react-dom"
 
 import App from "./App"
 
