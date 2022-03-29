988d85dd2dc5bdc17455213cf4a2cd00c3e07b36
shahzaibansari123 <ansarishahzaib567@gmail.com>
2022-03-29 23:48:59 +0500

    working on it

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
+import { CircularProgress,Select, Grid, Typography, Inputlabel, MenuItem, FormControl } from '@material-ui/core';
+import useStyles from './styles'
+
+
+
 
 export default function List() {
+  const classes = useStyles();
   return <div>List</div>;
 }
